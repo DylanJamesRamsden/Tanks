be1b6421c59fbe827350a15f5e77448bb0dcfba2
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-01-04 11:18:04 +0200

    Fixed another typo in the same comment in DTank

--- Source/Tanks/DTank.cpp
+++ Source/Tanks/DTank.cpp
@@ -40,7 +40,7 @@ void ADTank::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// Just storing a ref to the player controller as I don't want to have to regrab it everytime I need it
+	// Just storing a ref to the player controller as I don't want to have to refetch it everytime I need it
 	PlayerControllerRef = GetController<APlayerController>();
 	check(PlayerControllerRef);
 
